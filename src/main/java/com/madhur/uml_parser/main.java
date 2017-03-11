6dd178d94198f0b01d0caee285e4da5f0a71a3c9
Madhur Khandelwal <madhurkhandelwal234@gmail.com>
2017-03-10 18:31:22 -0800

    Hello world

---
+++ src/main/java/com/madhur/uml_parser/main.java
@@ -0,0 +1,12 @@
+package com.madhur.uml_parser;
+
+/**
+ * Hello world!
+ */
+public class Parcer_facade
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
