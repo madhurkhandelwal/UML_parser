60d9fc121c3afca9ede17734af66db0436788849
Madhur Khandelwal <madhurkhandelwal234@gmail.com>
2017-03-10 18:40:35 -0800

    Add unit test file

---
+++ src/test/java/com/madhur/uml_parser/main.java
@@ -0,0 +1,12 @@
+package com.madhur.uml_parser;
+
+import junit.framework.*;
+
+public class FacadeTest extends TestCase{
+	protected void setUp(){
+		//TODO
+	}
+	public void testMain(){
+		//TODO
+	}
+}
