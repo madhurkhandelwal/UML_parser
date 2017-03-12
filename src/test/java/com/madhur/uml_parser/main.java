4b2ab6bc15e83c583f4505b9f81522e876703d7d
Madhur Khandelwal <madhurkhandelwal234@gmail.com>
2017-03-11 20:15:37 -0800

    Add test constructor

--- src/test/java/com/madhur/uml_parser/main.java
+++ src/test/java/com/madhur/uml_parser/main.java
@@ -3,6 +3,10 @@ package com.madhur.uml_parser;
 import junit.framework.*;
 
 public class FacadeTest extends TestCase{
+	public TestCase(String testName)
+    	{
+        	super( testName );
+    	}
 	protected void setUp(){
 		//TODO
 	}
