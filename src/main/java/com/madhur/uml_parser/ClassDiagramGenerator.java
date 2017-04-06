b5a06e3b22982693271164121b136e35a86bed0e
Madhur Khandelwal <madhurkhandelwal234@gmail.com>
2017-04-05 17:03:45 -0700

    Generate a simple yuml string for each class
    TODO add methods, attributes, assosiations, inheritence, implementation

--- src/main/java/com/madhur/uml_parser/ClassDiagramGenerator.java
+++ src/main/java/com/madhur/uml_parser/ClassDiagramGenerator.java
@@ -1,20 +1,51 @@
 package com.madhur.uml_parser;
 
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
+import java.util.HashMap;
+import java.util.List;
+
 
 public class ClassDiagramGenerator{
     private String inputFolder, outputFolder;
+    private HashMap<String, Boolean> isClassOrInterface;
 
     public ClassDiagramGenerator(String inputFolder, String outputFolder){
         this.inputFolder = inputFolder;
         this.outputFolder = outputFolder;
+        isClassOrInterface = new HashMap<String, Boolean>();
     }
 
-    public void generate(){
+    public void generate() throws FileNotFoundException {
+        String[] javaFiles;
+        CompilationUnit cu;
+        String yumlString;
+
         System.out.println("Generating class diagram(s)...");
-        String[] javaFiles = getAllJavaFiles(inputFolder);
+        javaFiles = getAllJavaFiles(inputFolder);
+        for (String javaFile:javaFiles) {
+            System.out.println("Parsing " + javaFile + "...");
+            cu = JavaParser.parse(new File(inputFolder, javaFile));
+
+            // Assuming only classes or interfaces are present
+            List<TypeDeclaration<?>> classList = cu.getTypes();
+            for (Node n : classList) {
+                ClassOrInterfaceDeclaration coi = (ClassOrInterfaceDeclaration) n;
+                isClassOrInterface.put(coi.getNameAsString(), coi.isInterface());
+                System.out.print("Found class/interface " + coi.getNameAsString() + ": ");
+
+                yumlString = generateYUMLString(coi);
+            }
+        }
 
         /*
         lambda expression to get all cus
@@ -32,7 +63,7 @@ public class ClassDiagramGenerator{
     private static String[] getAllJavaFiles(String folder){
         // Return only the .java files present in the folder provided
         // TODO add support to walk through all folders
-        folder = folder.replaceFirst("^~", System.getProperty("user.home"));
+        System.out.println(folder);
         File inputFolder = new File(folder);
 
         // Using anonymous inner class just for fun
@@ -43,8 +74,23 @@ public class ClassDiagramGenerator{
             });
 
         // TODO use logging
-        for (String javaFile: javaFiles)
-            System.out.println(javaFile);
+//        for (String javaFile: javaFiles)
+//            System.out.println(javaFile);
         return javaFiles;
+    }
+
+    private String generateYUMLString(ClassOrInterfaceDeclaration coi){
+        String yumlString;
+        String interfaceStr="", name, attrs="", methods="";
+
+        if(coi.isInterface())
+        {
+            interfaceStr = "<<interface>>";
+        }
+        name = coi.getNameAsString();
+
+        yumlString = String.format("[%s%s|%s|%s]", interfaceStr, name, attrs, methods);
+        System.out.println(yumlString);
+        return yumlString;
     }
 }
